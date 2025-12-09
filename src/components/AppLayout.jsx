eb4d5136dd933ccfc7e41a2a761ee253a415ee2c
Fabien Moyon <fmoyon@linagora.com>
2025-12-09 09:54:38 +0100

    feat : Update Linshare logo in the header

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -3,12 +3,20 @@ import { Outlet } from 'react-router-dom'
 
 import { BarComponent, BarCenter } from 'cozy-bar'
 import BarTitle from 'cozy-ui/transpiled/react/BarTitle'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+
+import linshare from '../assets/icons/icon.svg'
 
 const AppLayout = () => {
   return (
     <>
       <BarCenter>
-        <BarTitle>LinShare</BarTitle>
+        <BarTitle>
+          <div className="u-flex u-flex-items-center">
+            <Icon icon={linshare} size="32" className="u-mr-half" />
+            LinShare
+          </div>
+        </BarTitle>
       </BarCenter>
       <BarComponent searchOptions={{ enabled: false }} />
       <Outlet />
